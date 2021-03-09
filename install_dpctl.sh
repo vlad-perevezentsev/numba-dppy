da0333c6ccde52401c0ce5ccac1067cd11085a94
vlad-perevezentsev <vladislav.perevezentsev@intel.com>
2021-03-09 11:11:29 -0600

    Fix numba-dppy dependencies

--- install_dpctl.sh
+++ install_dpctl.sh
@@ -2,6 +2,8 @@
 
 THEDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
 
+. ${THEDIR}/0.env.sh
+
 DPCTL_TARGET_VERSION=0.6.1
 echo ++++++++++++++++++ Build DPCTL ${DPCTL_TARGET_VERSION} +++++++++++++++++++
 git clone https://github.com/IntelPython/dpctl.git 

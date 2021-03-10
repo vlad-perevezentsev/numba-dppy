eca935e5cd27e49ec79480f8a80f5fdc821d0031
vlad-perevezentsev <vladislav.perevezentsev@intel.com>
2021-03-10 06:43:05 -0600

    Fix install_dpctl

--- install_dpctl.sh
+++ install_dpctl.sh
@@ -18,11 +18,16 @@
 
 THEDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
 
+. ${THEDIR}/0.env.sh
+
 DPCTL_TARGET_VERSION=0.5.0rc2
 echo ++++++++++++++++++ Build DPCTL ${DPCTL_TARGET_VERSION} +++++++++++++++++++
 git clone --branch ${DPCTL_TARGET_VERSION} https://github.com/IntelPython/dpctl.git 
 
 cd dpctl
+
+ls /opt/intel/oneapi/intelpython/latest/conda-bld/linux-64/dpctl
+ls /opt/intel/oneapi/intelpython/latest/
 
 # didn't find better way to set required version
 git tag -d $(git tag -l)

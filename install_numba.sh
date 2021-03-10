b7feae50e62dd7eb31f6ae071a28c3aac640edac
vlad-perevezentsev <vladislav.perevezentsev@intel.com>
2021-03-10 15:12:26 -0600

    Fix install_numba

--- install_numba.sh
+++ install_numba.sh
@@ -2,12 +2,9 @@
 
 THEDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
 
-DPCTL_TARGET_VERSION=0.52.0
-echo ++++++++++++++++++ Build NUMBA ${DPCTL_TARGET_VERSION} +++++++++++++++++++
+echo ++++++++++++++++++ Build NUMBA +++++++++++++++++++
 git clone https://github.com/IntelPython/numba.git
 
 cd numba
-
-git checkout ${DPCTL_TARGET_VERSION}
 
 python setup.py develop

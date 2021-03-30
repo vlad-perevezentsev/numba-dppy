60b972656157279cbb74868e227d80ef233f3aca
vlad-perevezentsev <vladislav.perevezentsev@intel.com>
2021-03-30 06:37:12 -0500

    Fix comments

--- scripts/0.env.sh
+++ scripts/0.env.sh
@@ -10,15 +10,6 @@ export ONEAPI_ROOT=/opt/intel/oneapi
 . ${ONEAPI_ROOT}/compiler/latest/env/vars.sh
 . ${ONEAPI_ROOT}/tbb/latest/env/vars.sh
 
-if true
-then
-    # Temporary use explicit version (arg_verz) due to MKLD-10520
-    arg_verz=latest
-    . ${ONEAPI_ROOT}/mkl/latest/env/vars.sh
-    unset arg_verz
-else
-    . ${ONEAPI_ROOT}/mkl/latest/env/vars.sh
-fi
 
 export DPCPPROOT=${ONEAPI_ROOT}/compiler/latest
 

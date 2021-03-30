7184fc5ffd6aa3bc1b0d028578d7633f8ac5965e
vlad-perevezentsev <vladislav.perevezentsev@intel.com>
2021-03-30 08:40:48 -0500

    Fix-5

--- scripts/install_dpctl.sh
+++ scripts/install_dpctl.sh
@@ -4,14 +4,14 @@ THEDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
 
 . ${THEDIR}/0.env.sh
 
-# DPCTL_TARGET_VERSION=0.5.0rc2
-DPCTL_TARGET_VERSION=gold/2021
+DPCTL_TARGET_VERSION=0.5.0rc2
+# DPCTL_TARGET_VERSION=gold/2021
 echo ++++++++++++++++++ Build DPCTL ${DPCTL_TARGET_VERSION} +++++++++++++++++++
 git clone --branch ${DPCTL_TARGET_VERSION} https://github.com/IntelPython/dpctl.git 
 
 cd dpctl
 
-# git tag ${DPCTL_TARGET_VERSION}
+git tag ${DPCTL_TARGET_VERSION}
 
 conda build conda-recipe/ --no-test -c ${ONEAPI_ROOT}/conda_channel
 
